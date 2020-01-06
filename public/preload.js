099fb0cde6ea0c85c4e8031a3adffd28f20ce45c
Matti Vesa <mattijvesa@gmail.com>
2020-01-06 15:46:34 +0200

    Initial commit

---
+++ public/preload.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+});
+
+window.Unide = {};
+window.Unide.inElectron = true;
+window.Unide.saveFile = (fileName, content) => {
+  const directory = fileName.substring(0, fileName.lastIndexOf("/"));
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+  fs.writeFileSync(fileName, content);
+};
+window.Unide.loadState = () => {
+  let state = '{"designs": {} }';
+  if (fs.existsSync("./src/main/resources/unide_state.json")) {
+    state = fs.readFileSync("./src/main/resources/unide_state.json", "utf8");
+  }
+  return state;
+};
+
+window.Unide.saveState = state => {
+  if (!fs.existsSync("./src/main/resources")) {
+    fs.mkdirSync("./src/main/resources", { recursive: true });
+  }
+  fs.writeFileSync("./src/main/resources/unide_state.json", state);
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Unide
+
+Unide is
+
+- Universal, as designs produced using it can be exported to many platforms
+- Unidirectional, as it is in complete control of its model and is capable of exporting it to other models
+- U and I, as it let's the user create pure Java Vaadin projects
+
+To run Unide locally via Electron, install it via npm `npm install -g unide` and then
+invoke `unide` at the root of your Vaadin project.
+
+[Unide can be accessed online](https://mjvesa.github.io/unide/)
+
+## Goal of the project
+
+Currently: the user is able to produce UIs, add basic navigation and finally export the
+result to a project using one of many frameworks, or no framework at all.
+
+Future: the user should be able to create full applications in Unide without programming.
